2b0c456c7089b0d5d30e279fe041bc08b84a2e0f
KaranKaran00 <kharakaran504@gmail.com>
2026-02-12 20:37:15 +0530

    import javax.swing

--- flappyBird/src/FlappyBird.java
+++ flappyBird/src/FlappyBird.java
@@ -1,2 +1,15 @@
-public class FlappyBird {
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class FlappyBird extends JPanel {
+    int boardsWidth =360;
+    int boardHeight =640;
+
+    FlappyBird(){
+        setPreferredSize(new Dimension(boardsWidth,boardHeight));
+        setBackground(Color.blue);
+    }
 }
