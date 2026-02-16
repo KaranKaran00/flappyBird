b3ec33ef71f6d6ee1711ba72b9b2efdde2218190
KaranKaran00 <kharakaran504@gmail.com>
2026-02-16 21:21:34 +0530

    loop

--- flappyBird/src/FlappyBird.java
+++ flappyBird/src/FlappyBird.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird extends JPanel {
+public class FlappyBird extends JPanel implements ActionListener {
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -14,25 +14,27 @@ public class FlappyBird extends JPanel {
     Image topPipeImg;
     Image bottomPipeImg;
 
-    //Bird
-    int birdX = boardWidth/8;
-    int birdY = boardHeight/2;
+    // Bird
+    int birdX = boardWidth / 8;
+    int birdY = boardHeight / 2;
     int birdWidth = 34;
     int birdHeight = 24;
 
     class Bird {
         int x = birdX;
         int y = birdY;
-        int width =birdWidth;
-        int height =birdHeight;
+        int width = birdWidth;
+        int height = birdHeight;
         Image img;
 
-        Bird(Image img){
+        Bird(Image img) {
             this.img = img;
         }
     }
-    //game logic
+
+    // game logic
     Bird bird;
+    Timer gameLoop;
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -44,8 +46,12 @@ public class FlappyBird extends JPanel {
         topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
         bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
 
-        //bird
+        // bird
         bird = new Bird(birdImg);
+
+        // game timer
+        gameLoop = new Timer(1000 / 60, this);// 1000/60 = 16.6
+        gameLoop.start();
     }
 
     public void paintComponent(Graphics g) {
@@ -54,10 +60,16 @@ public class FlappyBird extends JPanel {
     }
 
     public void draw(Graphics g) {
+        System.out.println("draw");
         // background
         g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
 
-        //bird
-        g.drawImage(bird.img, bird.x,bird.y,bird.width,bird.height,null);
+        // bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
     }
 }
