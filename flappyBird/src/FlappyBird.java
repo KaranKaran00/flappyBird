3ae5368f8179f78d25f7ba8c419469348f1e0c91
KaranKaran00 <kharakaran504@gmail.com>
2026-02-24 21:03:57 +0530

    game count

--- flappyBird/src/FlappyBird.java
+++ flappyBird/src/FlappyBird.java
@@ -62,8 +62,8 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
     Timer gameLoop;
     Timer placePipesTimer;
-
     boolean gameOver = false;
+    double score = 0;
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -130,6 +130,17 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
             Pipe pipe = pipes.get(i);
             g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
         }
+
+        //socre
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.PLAIN,32));
+        if(gameOver){
+            g.drawString("Game Over: " + String.valueOf((int) score),10,35);
+        }
+        else{
+            g.drawString(String.valueOf((int) score),10,35);
+        }
+
     }
 
     public void move() {
@@ -142,6 +153,11 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         for (int i = 0; i < pipes.size(); i++) {
             Pipe pipe = pipes.get(i);
             pipe.x += velocityX;
+
+            if(!pipe.passed && bird.x > pipe.x + pipe.width) {
+                pipe.passed = true;
+                score += 0.5;  //0.5 bacause there are 2 pipes! so 0.5*2 = 1,1 for each set of pipes
+            }
 
             if(collision(bird,pipe)){
                 gameOver = true;
