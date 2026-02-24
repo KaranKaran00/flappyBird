a8ce4ec04e7e53966646b377393b59001729e73a
KaranKaran00 <kharakaran504@gmail.com>
2026-02-24 19:18:22 +0530

    Game Stop

--- flappyBird/src/FlappyBird.java
+++ flappyBird/src/FlappyBird.java
@@ -142,10 +142,20 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         for (int i = 0; i < pipes.size(); i++) {
             Pipe pipe = pipes.get(i);
             pipe.x += velocityX;
+
+            if(collision(bird,pipe)){
+                gameOver = true;
+            }
         }
         if(bird.y > boardHeight){
             gameOver = true;
         }
+    }
+    public boolean collision(Bird a, Pipe b){
+        return a.x < b.x + b.width &&  //a's top left corner does't reach b's top right corner
+                a.x + a.width > b.x && //a's top right corner passes b's top left corner
+                a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
+                a.y + a.height > b.y;  //a's bottom left corner passes b's top left corner
     }
 
     @Override
